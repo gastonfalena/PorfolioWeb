bb9106e2012d6c246f8472b5c48cace5636f285c
gastonfalena <gastonfalena@gmail.com>
2025-02-14 18:58:38 -0300

    chore: add gihtub and linkedin bottom

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,24 @@
-import './header.css'
+import './header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 export default function Header() {
   return (
     <div className='container'>
-        <div className='item'><img className='imagen' src='/Me.png' alt='Gastón Falena'/></div>
-        <div>
+      <div className='item'>
+        <img className='imagen' src='/Me.png' alt='Gastón Falena' />
+      </div>
+      <div>
         <div className='item name'>Gastón Falena</div>
-        <div className='item talent'>Desarrollador de Sofwtare</div>
+        <div className='item talent'>Desarrollador de Software</div>
+        <div className="links">
+          <a href="https://www.linkedin.com/in/gaston-falena/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/gastonfalena" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
         </div>
-        
-        
-        {/* <div className='item'>¡Hola! Soy Gastón Falena, desarrollador de software especializado en tecnologías web. Estudio Ingeniería en Sistemas de Información y me especializo en React, TypeScript y Java con Spring, además del diseño de arquitecturas backend. Siempre estoy explorando nuevas tecnologías y mejores prácticas para crear aplicaciones modernas, seguras y de alto rendimiento.</div> */}
+      </div>
     </div>
-    
-  )
+  );
 }
